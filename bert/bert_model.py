1e1b22167f3a3f5169ff40c858d99ea2b57c5b56
WooSeok Kim <3suksw@gmail.com>
2025-11-15 17:51:22 -0500

    update: minor changes (description of `BertTagEmbeddings` and `n_epochs`)

--- bert/bert_model.py
+++ bert/bert_model.py
@@ -4,6 +4,14 @@ from transformers import AutoModel
 
 
 class BertTagEmbeddings(nn.Module):
+    """
+    Input: token embeddings size of 512
+    Output: embeddings size of 768
+    Given some token embedding vector including an information
+        of image tags and corresponding confidence scores information,
+        outputs an embedding vector representing visual captions of the image.
+    """
+
     def __init__(
         self,
         model_id: str = "bert-base-uncased",
